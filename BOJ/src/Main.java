0254dd035072aad9b68ca5b7b5fdf659162e7fd6
JunHo <u_innovation@naver.com>
2021-01-11 21:17:01 +0900

    2562

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,23 +1,20 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main { // 10818
+public class Main { // 2562
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
-		int n = scan.nextInt();
-		int[] arr = new int[n];
-
+		int[] arr = new int[9];
+		int max = 0;
+		int cnt = 0;
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = scan.nextInt();
+			
+			if (arr[i] > max) {
+				max = arr[i];
+				cnt = i + 1;
+			}
 		}
-		int min = arr[0];
-		int max = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] < min) min = arr[i];
-			if (arr[i] > max) max = arr[i];
-		}
-		System.out.println(min + " " + max);
-
+		System.out.println(max + "\n" + cnt);
 	}
 }
