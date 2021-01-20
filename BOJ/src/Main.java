c6999b66245ec8943e903f84addb61f1ad8ea020
JunHo <u_innovation@naver.com>
2021-01-20 20:51:11 +0900

    8958_StringBuilder

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,14 +1,24 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String[] arr = new String[scan.nextInt()];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = scan.next();
+		StringBuilder sb = new StringBuilder();
+		int test_case = Integer.parseInt(br.readLine()); // 테스트 케이스
+
+		String arr[] = new String[test_case];
+
+		for (int i = 0; i < test_case; i++) {
+			arr[i] = br.readLine();
+		}
+
+		for (int i = 0; i < test_case; i++) {
 			int cnt = 0;
 			int sum = 0;
+
 			for (int k = 0; k < arr[i].length(); k++) {
 				if (arr[i].charAt(k) == 'O') {
 					cnt++;
@@ -17,8 +27,9 @@ public class Main {
 				}
 				sum += cnt;
 			}
-			System.out.println(sum);
-
+			sb.append(sum).append("\n");
 		}
+		System.out.print(sb);
+
 	}
 }
