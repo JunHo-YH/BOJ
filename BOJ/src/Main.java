de88323105f350faca43a2754677d01aa3e20456
JunHo <u_innovation@naver.com>
2021-01-20 17:17:12 +0900

    3052_HashSet

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,20 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-//import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int[] arr = new int[10];
-		int val = Integer.parseInt(bf.readLine()) * Integer.parseInt(bf.readLine()) * Integer.parseInt(bf.readLine());
-		String str = String.valueOf(val);	
+	public static void main(String[] args) {
 
-		for (int i = 0; i < str.length(); i++) {
-			arr[(str.charAt(i) - '0')]++;
+		Scanner scan = new Scanner(System.in);
+		HashSet<Integer> h = new HashSet<Integer>();
+
+		for (int i = 0; i < 10; i++) {
+			h.add(scan.nextInt() % 42);
 		}
-		for (int v : arr) {
-			System.out.println(v);
-		}
+
+		scan.close();
+		System.out.println(h.size());
+
 	}
 }
