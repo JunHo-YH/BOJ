0db116de2e4f1db5340fd72bfe93cebf9d4a4dde
JunHo <u_innovation@naver.com>
2021-01-20 20:38:29 +0900

    8958_Scanner

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -3,20 +3,22 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] arr = new int[scan.nextInt()];
+		String[] arr = new String[scan.nextInt()];
 
-		int max = 0;
-		double total = 0;
-		double score = 0;
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = scan.nextInt();
-			if (arr[i] > max)
-				max = arr[i];
-		}
-		for (int i = 0; i < arr.length; i++) {
-			total += (arr[i] / (double) max) * 100 / arr.length;
-		}
-		System.out.println(total);
+			arr[i] = scan.next();
+			int cnt = 0;
+			int sum = 0;
+			for (int k = 0; k < arr[i].length(); k++) {
+				if (arr[i].charAt(k) == 'O') {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+				sum += cnt;
+			}
+			System.out.println(sum);
 
+		}
 	}
 }
