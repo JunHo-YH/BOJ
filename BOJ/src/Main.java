ef8abcba61cbb64635eadca95177e67e5bef1e34
JunHo <u_innovation@naver.com>
2021-01-20 19:09:44 +0900

    3052_BufferedReader

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,17 +1,21 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		HashSet<Integer> h = new HashSet<Integer>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean[] arr = new boolean[42];
 
 		for (int i = 0; i < 10; i++) {
-			h.add(scan.nextInt() % 42);
+			arr[Integer.parseInt(br.readLine()) % 42] = true;
 		}
 
-		scan.close();
-		System.out.println(h.size());
-
+		int count = 0;
+		for (boolean value : arr) {
+			if (value)
+				count++;
+		}
+		System.out.println(count);
 	}
 }
