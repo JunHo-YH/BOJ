ea2ee45636feb3c52e8b6bb36bfe045e1d3318c8
JunHo <u_innovation@naver.com>
2021-03-30 23:06:39 +0900

    대소문자 변환

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-	public int solution(String str, char t) {
-		int answer = 0;
-		str = str.toUpperCase(); // 대문자로 변환
-		t = Character.toUpperCase(t); // 대문자로 변환
-		
-		for (char c : str.toCharArray()) {
-			if (c == t) answer++;
+	public String solution(String str) {
+		String answer = "";
+		for (char x : str.toCharArray()) {
+			if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+			else answer += Character.toLowerCase(x);
 		}
-		
 		return answer;
 	}
-	
-	
 	public static void main(String[] args) {
-		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
 		String str = scan.next();
-		char c = scan.next().charAt(0);
-		System.out.println(T.solution(str, c));
+		
+		Main T = new Main();
+		System.out.println(T.solution(str));
+		
 	
 	}
 }
