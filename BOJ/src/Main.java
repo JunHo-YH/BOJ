ece8ce65ca5aa3e8ce143ccccb36540ef6cc9d8e
JunHo <u_innovation@naver.com>
2021-04-01 02:52:12 +0900

    3번_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 public class Main {
 	public String solution(String str) {
 		String answer = "";
-		for (char x : str.toCharArray()) {
-			if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-			else answer += Character.toLowerCase(x);
+		int m = Integer.MIN_VALUE, pos; // 최소값으로 초기화
+		while((pos = str.indexOf(' ')) != -1) {
+			String tmp = str.substring(0, pos);
+			int len = tmp.length();
+			if (len > m) {
+				m = len;
+				answer = tmp;
+			}
+			str = str.substring(pos+1);
+			if(str.length() > m) answer = str;
 		}
 		return answer;
 	}
+	
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String str = scan.next();
-		
+		String str = scan.nextLine();
 		Main T = new Main();
 		System.out.println(T.solution(str));
-		
+	
 	
 	}
 }
