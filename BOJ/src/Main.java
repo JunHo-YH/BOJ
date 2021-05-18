b2d8b42aa654bc348c6c6252a4bf2c63efc87ebe
JunHo <u_innovation@naver.com>
2021-05-18 15:26:27 +0900

    문자열 찾기 복습(2)

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		Scanner in2 = new Scanner(System.in);
+	public int solution(String str, char c) {
+		int answer = 0;
 		
-		String str = in.next().toUpperCase();
-		char chr = in.next().toUpperCase().charAt(0);
-		
-		int cnt = 0;
+		str = str.toUpperCase();
+		c = Character.toUpperCase(c);
 		if(str.length() < 100) {
 			for(int i = 0; i < str.length(); i++) {
-				if(str.charAt(i) == chr) {
-					cnt++;
+				if(str.charAt(i) == c) {
+					answer++;
 				}
 			}
+			return answer;
 		}else {
-			return;
+			return -1;
 		}
-		System.out.println(cnt);
+	}
+	public static void main(String[] args) {
+		Main m = new Main();
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+		char c = scan.next().charAt(0);
+		
+		System.out.println(m.solution(str, c));
 	}
 
 
