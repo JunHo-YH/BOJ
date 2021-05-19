b75043addaa32fc479d7620855873ea09503593f
JunHo <u_innovation@naver.com>
2021-05-19 20:19:53 +0900

    프로그래머스

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,37 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-	public String solution(String str) {
-		
-		String[] arrStr = str.split(" ");
-		int strLen = 0;
-		for(String len : arrStr) {
-			if(len.length() > strLen) {
-				strLen = len.length();				
-			}
-			
-		}
-		for(String len : arrStr) {
-			if(len.length() == strLen) {
-				str = len;
-			}
-		}
-		
-		
-		return str;
-	}
-	
-	public static void main(String[] args) {
-		Main m = new Main();
-		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
-		if(str.length() < 100) {
-			System.out.println(m.solution(str));
-		}
-		
-	}
-
-
-
-	
+class Main {
+    
+    public String solution(String[] seoul) {
+        
+    	List<String> arrList = Arrays.asList(seoul);
+        int index = arrList.indexOf("Kim");        
+        String answer = "김서방은 " + index + "에 있다";
+        
+        
+        
+        return answer;
+        
+        
+        
+    }
 }
