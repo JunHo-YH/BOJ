66cdb667769f0deeb1f3a068d2314d4c60bad286
Junho <u_innovation@naver.com>
2021-05-23 22:12:18 +0900

    연필 개수_self

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,10 +1,10 @@
-function solution(a, b, c) {
-    let answer = "YES", max;
-    let sum = a+b+c;
-    if(a>b) max = a;
-    else max = b;
-    if(c > max) max = c;
-    if((sum-max) <= max) answer = "NO";
-    return answer;
+function solution(student) {
+    answer = student / 12;
+    if(student % 12 >= 1) {
+        parseInt(answer++);
+
+    }
+    return parseInt(answer);
 }
-console.log(solution(6,7,11));
+
+console.log(solution(178))
