f8e13ed2630c4b1309eb7f05a4644b29bef84c53
Junho <u_innovation@naver.com>
2021-05-23 19:47:14 +0900

    최솟값 구하기

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,1 +1,9 @@
-console.log("Hello Node.js!");
+function solution(a,b,c) {
+    let answer;
+    if(a < b) answer = a;
+    else answer = b;
+    if(c < answer) answer = c;
+    return answer;
+}
+
+console.log(solution(6, 5, 11));
