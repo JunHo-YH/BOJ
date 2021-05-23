60d37c2951180db434b92e1b00ab623f1eba0114
Junho <u_innovation@naver.com>
2021-05-23 21:39:42 +0900

    삼각형 판별_내 방식

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,9 +1,28 @@
-function solution(a,b,c) {
-    let answer;
-    if(a < b) answer = a;
+function solution(a, b, c) {
+    answer = '';
+    if(a > b) answer = a;
     else answer = b;
-    if(c < answer) answer = c;
+    if(c > answer) answer = c;
+
+    if(answer === a) {
+        if(answer < b + c) {
+            return answer = "Yes";
+        }else {
+            return answer = "NO";
+        }
+    } else if(answer === b) {
+        if(answer < a + c) {
+            return answer = "Yes";
+        }else {
+            return answer = "NO";
+        }
+    }else {
+        if(answer < b + c) {
+            return answer = "Yes";
+        }else {
+            return answer = "NO";
+        }
+    }
     return answer;
 }
-
-console.log(solution(6, 5, 11));
+console.log(solution(13,33,17));
