4eb6228a07cf7d7f69382861dd57135382bef694
Junho <u_innovation@naver.com>
2021-05-24 21:36:14 +0900

    바로 앞 수보다 큰 수만 출력

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,10 +1,15 @@
 function solution(arr) {
-    let answer;
-    answer = arr.filter((v,i) => {
-        if(arr.indexOf(v) === i) return v;
-    });
-    
+    let answer = [];
+    answer.push(arr[0]);
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] > arr[i-1]) answer.push(arr[i])
+    }
+
+
     return answer;
+
+
 }
-let arr = ["good", "time", "good", "time", "student"];
+
+let arr = [7,3,9,5,6,12];
 console.log(solution(arr));
