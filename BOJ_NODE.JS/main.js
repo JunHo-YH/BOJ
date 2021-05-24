c7c6d5b267ddc0ebcdce8bbd2eaad943eac24fbd
Junho <u_innovation@naver.com>
2021-05-24 11:47:10 +0900

    A를 #으로, replace/ g

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,16 +1,8 @@
-function solution(arr) {
-    let sum = arr.reduce((a,b)=>a+b, 0);
-    for(let i = 0; i < arr.length-1; i++) {
-        for(let j = i+1; j < arr.length; j++) {
-            if(sum - (arr[i] + arr[j]) === 100) {
-                arr.splice(j,1);
-                arr.splice(i,1);
-            }
-        }
-    }
-    return arr;    
+function solution(str) {
+    let answer = "";
+    answer = str.replace(/A/g, '#')
+    return answer;
 
 }
-
-let arr = [20,7,23,19,10,15,25,8,13];
-console.log(solution(arr));
+let str = "BANANA";
+console.log(solution(str))
