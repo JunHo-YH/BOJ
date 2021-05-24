af0bf93eb2aa7f61c05e731dee3ffb6f493fc5c1
Junho <u_innovation@naver.com>
2021-05-24 11:32:32 +0900

    일곱난쟁이_reduce

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,10 +1,17 @@
-function solution(day, cars) {
-    let ban = 0;
-    for(let x of cars) {
-        if(x % 10 === day) ban++;
+function solution(arr) {
+    let answer = arr;
+    let sum = arr.reduce((a,b)=>a+b, 0);
+    for(let i = 0; i < arr.length-1; i++) {
+        for(let j = i+1; j < arr.length; j++) {
+            if(sum - (arr[i] + arr[j]) === 100) {
+                arr.splice(j,1);
+                arr.splice(i,1);
+            }
+        }
     }
-    return ban;
+    return answer;    
+
 }
-day = 0;
-cars = [12,20,54,30,87,91,30];
-console.log(solution(day, cars));
+
+let arr = [20,7,23,19,10,15,25,8,13];
+console.log(solution(arr));
