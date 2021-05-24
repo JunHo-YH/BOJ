07546a67d28e2787a1dba1a621f5e6224823e4a8
Junho <u_innovation@naver.com>
2021-05-24 12:55:04 +0900

    대문자 찾기_아스키코드

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,9 +1,9 @@
 function solution(str) {
     let answer = 0;
     for(let x of str) {
-        if(x.match(/[A-Z]/)) {
-            answer++;
-        }
+        let num = x.charCodeAt();
+        if(num >= 65 && num <= 90) answer++;
+        // if(x === x.toUpperCase()) answer++;
     }
     
     return answer;
