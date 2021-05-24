5dc3311481a0826481615dd5284ce607937054a2
Junho <u_innovation@naver.com>
2021-05-24 16:52:15 +0900

    중복된 문자 갯수_indexOf

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,10 +1,11 @@
 function solution(str) {
-    let answer = "";
-    // console.log(str.indexOf('k', 1));
-    for(let i = 0; i < str.length; i++) {
-        console.log(str[i], i, str.indexOf(str[i]));
-        if(str.indexOf(str[i]) === i) answer += str[i];
-    }
+    let answer = 0;
+    let pos = str.indexOf('k');
+    while(pos !== -1) {
+        answer++;
+        pos = str.indexOf('k', pos+1);
+    }   
+
     return answer;
 }
 let str = "ksekkset";
