3e749c3e6b43808f93651fa2ce5fef816130e8c0
Junho <u_innovation@naver.com>
2021-05-24 10:07:36 +0900

    홀수_for of

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,14 +1,19 @@
 function solution(arr) {
+    let answer = [];
     let sum = 0;
-    let oddArr = [];
-    arr.forEach(num => {
-        if(num % 2 == 1 ) {
-            sum += num;
-            oddArr.push(num);
-        };
-    });
+    for(let x of arr) {
+        if(x % 2 === 1) {
+            sum += x;
+            answer.push(x);
+        }
+    }
+
     console.log(sum);
-    console.log(Math.min(...oddArr));
-};
+    console.log(Math.min(...answer));
+
+
+
+}
+
 let arr = [12,77,38,41,53,92,85];
 solution(arr);
