9cf3584a086b2636d03da539d251d42540a9cbbd
Junho <u_innovation@naver.com>
2021-05-24 23:20:45 +0900

    보이는 학생

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,15 +1,17 @@
 function solution(arr) {
-    let answer = [];
-    answer.push(arr[0]);
+    let answer = 1, max = arr[0]; 
     for(let i = 1; i < arr.length; i++) {
-        if(arr[i] > arr[i-1]) answer.push(arr[i])
+        if(arr[i] >  max) {
+            answer++;
+            max = arr[i];
+        }
     }
+    // answer = answer.length;
 
 
     return answer;
 
-
 }
 
-let arr = [7,3,9,5,6,12];
+let arr = [130,135,148,140,145,150,150,153];
 console.log(solution(arr));
