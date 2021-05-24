e7d0f4987edf3add04bb639cf7b8286a5fef991c
Junho <u_innovation@naver.com>
2021-05-24 14:32:12 +0900

    대<->소 문자 바꾸기

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,13 +1,10 @@
 function solution(str) {
     let answer = "";
     for(let x of str) {
-        let num = x.charCodeAt(); //아스키 번호로 변환
-        if(num >= 97 && num <= 122) answer += String.fromCharCode(num-32);
-        else answer += x;
-        // if(x === x.toLowerCase()) answer += x.toUpperCase();
-        // else answer += x;
+        if(x === x.toLowerCase()) answer += x.toUpperCase();
+        else answer += x.toLowerCase();
     }
     return answer;
 }
-let str = "ItisTimeToStudy";
+let str = "StuDY";
 console.log(solution(str));
