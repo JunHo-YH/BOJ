a9d859b19da6bd23add8dbb7d1cdcd889ed3ff67
Junho <u_innovation@naver.com>
2021-05-25 00:25:53 +0900

    가위바위보

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,14 +1,14 @@
 function solution(A, B) {
+    let answer = "";
     for(let i = 0; i < A.length; i++) {
-        if(A[i] === 1) {
-            console.log(answer = B[i] === 1 ? "D" : B[i] === 2 ? "B" : "A"); 
-        }else if(A[i] === 2) {
-            console.log(answer = B[i] === 2 ? "D" : B[i] === 1 ? "A" : "B");
-        }else {
-            console.log(answer = B[i] === 3 ? "D" : B[i] === 1 ? "B" : "A");
-        }
+        if(A[i] === B[i]) answer += 'D' + ' ';
+        else if(A[i] === 1 && B[i] === 3) answer +='A' + ' ';
+        else if(A[i] === 2 && B[i] === 1) answer +='A' + ' ';
+        else if(A[i] === 3 && B[i] === 2) answer +='A' + ' ';
+        else answer += 'B' + ' ';
     }
+    return answer;
 }
 let A = [2,3,3,1,3];
 let B = [1,1,2,2,3];
-solution(A,B);
+console.log(solution(A,B));
