422f620380ae7ec19dfb5bfce770863f93fb945d
Junho <u_innovation@naver.com>
2021-05-24 15:43:08 +0900

    ""

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,8 +1,8 @@
 function solution(str) {
     let answer ="";
     let mid = Math.floor(str.length / 2) // 몫만 출력
-    if(str.length %2 === 1) answer = s.substring(mid, mid+1);
-    else answer = str.substring(mid-1, mid+1);
+    if(str.length %2 === 1) answer = s.substr(mid, 1);
+    else answer = str.substr(mid-1, 2);
     return answer;
 }
 let str = "good";
