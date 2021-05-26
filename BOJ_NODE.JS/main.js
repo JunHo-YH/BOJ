559d1d86977e461d11e9e6e34affb1b0c436808e
Junho <u_innovation@naver.com>
2021-05-26 12:04:27 +0900

    회문문자열

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,31 +1,11 @@
-function solution(arr) {
-    let answer = 0;
-    let n = arr.length;
-    let dx = [-1,0,1,0];
-    let dy = [0,1,0,-1];
-    
-    for(let i = 0; i < n; i++) {
-        for(let j = 0; j < n; j++) {
-            let flag = 1;
-            for(let k = 0; k < 4; k++) {
-                let nx = i+dx[k];
-                let ny = j+dy[k]
-                if(nx>=0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
-                    flag = 0;
-                    break;
-                }
-            }5
-            if(flag) answer++;
-        }
+function solution(str) {
+    let answer = "YES";
+    str.toUpperCase();
+    let len = str.length;
+    for(let i = 0; i < Math.floor(len / 2); i++) {
+        if(str[i] !== str[len-i-1]) return answer = "NO";
     }
-
-
     return answer;
 }
-
-let arr=[[5, 3, 7, 2, 3], 
-         [3, 7, 1, 6, 1],
-         [7, 2, 5, 3, 4],
-         [4, 3, 6, 4, 1],
-         [8, 7, 3, 5, 2]];
-console.log(solution(arr));
+str ="goOoG";
+console.log(solution(str));
