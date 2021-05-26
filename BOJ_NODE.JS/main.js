a4483f28c907f949c5c61ab1d909dfd99fd228bb
Junho <u_innovation@naver.com>
2021-05-26 17:32:52 +0900

    유효한 팰린드롬(정규식)

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,6 +1,5 @@
 function solution(str) {
     let answer = "Yes";
-    let arr2 = [];
     str = str.toLowerCase().replace(/[^a-z]/g, '');
     if(str.split('').reverse().join('') !== str) return "NO"; 
     return answer;
