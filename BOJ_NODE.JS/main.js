105cd9ffc6d95bdec3018012476a8f674fece8c1
Junho <u_innovation@naver.com>
2021-05-26 12:31:10 +0900

    메서드를 이용한 회전문자열

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,10 +1,7 @@
 function solution(str) {
     let answer = "YES";
     str = str.toUpperCase();
-    let len = str.length;
-    for(let i = 0; i < Math.floor(len / 2); i++) {
-        if(str[i] !== str[len-i-1]) return "NO";
-    }
+    if(str.split('').reverse().join('') !== str) return "NO";
     return answer;
 }
 str ="goooG";
