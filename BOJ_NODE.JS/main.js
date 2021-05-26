5cd9692b64f38af0e16794fe8832fb5a49da89a3
Junho <u_innovation@naver.com>
2021-05-26 11:10:43 +0900

    봉우리

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,29 +1,31 @@
 function solution(arr) {
-    let answer = Number.MIN_SAFE_INTEGER;
+    let answer = 0;
     let n = arr.length;
-    let sum1 = sum2 = 0;
+    let dx = [-1,0,1,0];
+    let dy = [0,1,0,-1];
+    
     for(let i = 0; i < n; i++) {
-        sum1 = sum2 = 0;
         for(let j = 0; j < n; j++) {
-            sum1 += arr[i][j]; // 행 총합
-            sum2 += arr[j][i]; // 열 총합
+            let flag = 1;
+            for(let k = 0; k < 4; k++) {
+                let nx = i+dx[k];
+                let ny = j+dy[k]
+                if(nx>=0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                    flag = 0;
+                    break;
+                }
+            }5
+            if(flag) answer++;
         }
-        answer = Math.max(answer, sum1, sum2);
-        
     }
-    sum1 = sum2 = 0;
-    // 대각선 총합 구하기
-    for(let i = 0; i < n; i++) {
-        sum1 += arr[i][i];
-        sum2 += arr[i][n-i-1];
-    }
-    answer = Math.max(answer, sum1, sum2);
+
 
     return answer;
 }
-let score = [[10,13,10,12,15],
-             [12,39,30,23,11],
-             [11,25,50,53,15],
-             [19,27,29,37,27],
-             [19,13,30,13,19]];
-console.log(solution(score));
+
+let arr=[[5, 3, 7, 2, 3], 
+         [3, 7, 1, 6, 1],
+         [7, 2, 5, 3, 4],
+         [4, 3, 6, 4, 1],
+         [8, 7, 3, 5, 2]];
+console.log(solution(arr));
