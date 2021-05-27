3f3fdb02e94873daefb4ade4adc382468e593e4c
Junho <u_innovation@naver.com>
2021-05-27 09:47:06 +0900

    가장 짧은 문자거리

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,7 +1,26 @@
-function solution(str) {
-    str = str.toLowerCase().replace(/[^0-9]/g,'');
-    return answer = parseInt(str);
-    
+function solution(str, t) {
+    let answer = [];
+    let p = 1000;
+    for(let x of str) {
+        if(x === t) {
+            p = 0;
+            answer.push(p);
+        } else {
+            p++;
+            answer.push(p);
+        }
+    }
+    p = 1000;
+    for(let i = str.length-1; i >=0; i--) {
+        if(str[i] === t) p = 0;
+        else {
+            p++;
+            answer[i] = Math.min(answer[i], p);
+        }
+    }
+
+    return answer;
 }
-let str = "g0en2T0s8eSoft";
-console.log(solution(str));
+let str = "teachermode";
+let t = "e"; 
+console.log(solution(str,t));
