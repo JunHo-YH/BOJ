a91153a5be4c293314a71ae429aac9d06f3bcbda
Junho <u_innovation@naver.com>
2021-06-02 20:53:22 +0900

    홀수_복습

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,8 +1,15 @@
-function solution(n) {
-    let sum = 0;
-    for(let i = 1; i <= n; i++) {
-        sum += i;
+function solution(arr) {
+    let answer = [];
+    let sum = 0, min = Number.MAX_SAFE_INTEGER;
+    for(let x of arr) {
+        if(x % 2 === 1) {
+            sum += x;
+            if(x < min) min = x;
+        }
     }
-    return sum;
+    answer.push(sum);
+    answer.push(min);
+    return answer;
 }
-console.log(solution(10));
+let arr = [12,77,38,41,53,92,85];
+console.log(solution(arr));
