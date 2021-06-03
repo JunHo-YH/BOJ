1687dfd89f62df5abbd95a1fb78911edb9133c8b
Junho <u_innovation@naver.com>
2021-06-03 11:10:48 +0900

    대소문자 변환_복습

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,12 +1,11 @@
 function solution(str) {
     let answer = "";
     for(let x of str) {
-        let num = x.charCodeAt(); // ASCII NUMBER
-        if(num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
-        else answer += x;
+        if(x === x.toLowerCase()) answer += x.toUpperCase();
+        else answer += x.toLowerCase();
     }
-
     return answer;
 }
-let str = "ItisTimeToStudy";
+
+let str = "StuDY";
 console.log(solution(str));
