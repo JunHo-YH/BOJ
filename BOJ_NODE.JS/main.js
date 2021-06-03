ce259883a89e5bf1aad6216694ccaf7eae84dcb7
Junho <u_innovation@naver.com>
2021-06-03 09:52:45 +0900

    문자 찾기_복습

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,17 +1,10 @@
-function solution(arr) {
-    let answer = arr;
-    let sum = arr.reduce((a,b)=>a+b, 0)
-    for(let i = 0; i < arr.length-1; i++) {
-        for(let k = i+1; k < arr.length; k++) {
-            if(sum - (arr[i] + arr[k]) === 100) {
-                arr.splice(k,1);
-                arr.splice(i,1);
-            }
-        }
+function solution(str, find) {
+    let answer = 0;
+    for(let x of str) {
+        if(x === find) answer++;
     }
-
     return answer;
 }
-let arr = [20,7,23,19,10,15,25,8,13];
-console.log(solution(arr));
-
+let str = "COMPUTERPROGRAMMING";
+let find = "R";
+console.log(solution(str,find));
