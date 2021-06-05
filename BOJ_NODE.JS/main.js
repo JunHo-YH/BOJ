86cd16d1e47e95a53d838521d174c48ed610ac64
Junho <u_innovation@naver.com>
2021-06-05 23:53:51 +0900

    seoul

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,9 +1,5 @@
-function solution(arr) {
-    let answer;
-    answer = arr.filter((v,i)=>{
-        if(arr.indexOf(v) === i) return v;
-    })
-    return answer;
-}
-let arr = ['good', 'time', 'good', 'time', 'student'];
-console.log(solution(arr));
+function solution() {
+
+};
+console.log(solution());
+
