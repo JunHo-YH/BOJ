7d7ac33598a9dfbe70a8d1c8631fe91ff435a7ec
Junho <u_innovation@naver.com>
2021-06-09 23:41:14 +0900

    resume

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,13 +1,5 @@
 function solution(arr) {
-    let answer = 1;
-    let tall = arr[0];
-    for(let i = 1; i < arr.length; i++) { 
-        if(arr[i] > tall) {
-            tall = arr[i]; // 가장 큰 키 갱신
-            answer++;
-        }        
-    } 
-    
+    let answer;
     return answer;
 }
 let arr = [130,135,148,140,145,150,150,153];
