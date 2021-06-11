e1a0ca360560c9818da481f2e4a6f60bd4ebfd1c
Junho <u_innovation@naver.com>
2021-06-12 00:44:57 +0900

    유요한 팰린드롬

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,8 +1,13 @@
 function solution(str) {
     let answer="Yes";
-    str = str.toUpperCase();
-    str = str.replace(/[^A-Z]/g,'');// 정규식으로 문자열만 남기기
-    if(str.split('').reverse().join('') !== str) return "NO";
+    str = str.toUpperCase().replace(/[^A-Z]/g,'');
+    let len = str.length;
+    // str = str.replace(/[^A-Z]/g,'');// 정규식으로 문자열만 남기기
+    // if(str.split('').reverse().join('') !== str) return "NO";
+    for(let i = 0; i < Math.floor(len / 2); i++) {
+        if(str[i] !== str[len -i -1]) return "NO";
+    }
+    
     return answer;
 }
 let str="found7, time: study; Yduts; emit, 7Dnuof";
