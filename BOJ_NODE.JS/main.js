65354ca6d758f7b5101cba8903004975a2b9fcf9
Junho <u_innovation@naver.com>
2021-06-11 12:20:11 +0900

    등수 구하기_복습(Array.from)

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,17 +1,14 @@
-function solution(arrA, arrB) {
-    let answer = [];
-    for(let i = 0; i < arrA.length; i++) {
-        for(let k = i; k === i; k++) {
-            if(arrA[i] === arrB[k]) answer.push("D");
-            else if(arrA[i] === 1 && arrB[k] === 3) answer.push("A");
-            else if(arrA[i] === 2 && arrB[k] === 1) answer.push("A");
-            else if(arrA[i] === 3 && arrB[k] === 2) answer.push("A");
-            else answer.push("B");
-
+function solution(score) {
+    let n = score.length;
+    let answer = Array.from({length:n}, ()=>1); // [1, 1, 1, 1, 1] 초기화
+    for(let i = 0; i < n; i++) {
+        for(let k = 0; k < n; k++) {
+            if(score[i] < score[k]) answer[i]++;
         }
     }
+
     return answer;
 }
-let arrA = [2,3,3,1,3];
-let arrB = [1,1,2,2,3];
-console.log(solution(arrA,arrB))
+    
+let score = [87,89,92,100,76];
+console.log(solution(score));
