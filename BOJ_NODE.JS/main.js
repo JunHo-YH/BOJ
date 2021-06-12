e630f34a3dd2c93295e682ed7d153ea635436f41
Junho <u_innovation@naver.com>
2021-06-12 15:15:02 +0900

    자릿수 합

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,9 +1,10 @@
-function solution(str) {
-    let answer="";
-    for(let x of str) {
-        if(!isNaN(x)) answer += x;
+function solution(arr) {
+    let answer, max = Number.MIN_SAFE_INTEGER;
+    for(let x of arr) {
+        let sum = x.toString().split('').reduce((a,b)=>a + Number(b), 0)
+        console.log(sum)
     }
-    return parseInt(answer);
+    return answer;
 }
-let str="g0en2T0s8eSoft";
-console.log(solution(str));
+let arr = [128,460,603,40,521,137,123];
+console.log(solution(arr));
