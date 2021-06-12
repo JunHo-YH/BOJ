c6ea4ae1d4a7098cef9c4099d01e7592724aab7a
Junho <u_innovation@naver.com>
2021-06-13 01:07:46 +0900

    공통원소 구하기

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,18 +1,18 @@
 function solution(arr1, arr2) {
     let answer = [];
-    let n = arr1.length;
-    let m = arr2.length;
+    arr1.sort();
+    arr2.sort();
     let p1 = p2 = 0;
-    while(p1 < n && p2 < m) { // 동일한 위치까지 비교
-        if(arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]); // arr1[p1]값을 넣고 p1++;
-        else answer.push(arr2[p2++]);
-    };
-    // 나머지는 비교하지 않고 넣기
-    while(p1 < n) answer.push(arr1[p1++]);
-    while(p2 < m) answer.push(arr2[p2++]);
+    while(p1 < arr1.length && p2 < arr2.length) {
+        if(arr1[p1] === arr2[p2]) {
+            answer.push(arr1[p1++]);
+            p2++
+        }else if(arr1[p1] < arr2[p2]) p1++;
+        else p2++; 
+    }
     return answer;
 }
-let a = [1,3,5];
-let b = [2,3,6,7,9];
-console.log(solution(a,b))
 
+let a = [1,3,9,5,2];
+let b = [3,2,5,7,8];
+console.log(solution(a, b));
