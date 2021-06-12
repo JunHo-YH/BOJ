064291d00bcedfbc9f3d5ec8c56aa49cbdf0fc65
Junho <u_innovation@naver.com>
2021-06-12 09:34:03 +0900

    숫자만 추출_복습

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,14 +1,9 @@
 function solution(str) {
-    let answer="Yes";
-    str = str.toUpperCase().replace(/[^A-Z]/g,'');
-    let len = str.length;
-    // str = str.replace(/[^A-Z]/g,'');// 정규식으로 문자열만 남기기
-    // if(str.split('').reverse().join('') !== str) return "NO";
-    for(let i = 0; i < Math.floor(len / 2); i++) {
-        if(str[i] !== str[len -i -1]) return "NO";
+    let answer="";
+    for(let x of str) {
+        if(!isNaN(x)) answer += x;
     }
-    
-    return answer;
+    return parseInt(answer);
 }
-let str="found7, time: study; Yduts; emit, 7Dnuof";
+let str="g0en2T0s8eSoft";
 console.log(solution(str));
