024ad45ac130e35bfeb4c7364a0e330e48f7ff0e
Junho <u_innovation@naver.com>
2021-06-13 10:02:29 +0900

    연속 부분수열 1

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,18 +1,16 @@
-function solution(arr1, arr2) {
-    let answer = [];
-    arr1.sort();
-    arr2.sort();
-    let p1 = p2 = 0;
-    while(p1 < arr1.length && p2 < arr2.length) {
-        if(arr1[p1] === arr2[p2]) {
-            answer.push(arr1[p1++]);
-            p2++
-        }else if(arr1[p1] < arr2[p2]) p1++;
-        else p2++; 
+function solution(m, arr1) {
+    let answer = 0, lt = 0, sum = 0;
+    for(let rt = 0; rt < arr1.length; rt++) {
+        sum += arr1[rt];
+        if(sum === m) answer++;
+        while(sum >= m) {
+            sum -= arr1[lt++];
+            if(sum === m) answer++;
+        }
     }
-    return answer;
-}
 
-let a = [1,3,9,5,2];
-let b = [3,2,5,7,8];
-console.log(solution(a, b));
+    return answer;
+};
+
+let a = [1,2,1,3,1,1,1,2]
+console.log(solution(6, a));
