04c12137d2513c06438e20d6aa36f3e0fde13ba0
Junho <u_innovation@naver.com>
2021-06-16 17:14:42 +0900

    디버깅

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,20 +1,19 @@
 function solution(arr) {
-    let answer = 0, max = Number.MIN_VALUE;
+    let answer = 0;
+    let max = Number.MIN_SAFE_INTEGER;
     for(let x of arr) {
         let sum = 0, tmp = x;
         while(tmp) {
             sum += (tmp % 10);
-            tmp = Math.floor(tmp / 10);
+            tmp = (Math.floor(tmp / 10));
         }
         if(sum > max) {
             max = sum;
             answer = x;
         }else if(sum === max) {
-            if(x > answer) answer = x;
+            if(x > answer) answer = x; 
         }
     }
-
-
 
     return answer;
 }
