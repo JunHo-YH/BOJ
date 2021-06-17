56ace9b561557d1844dd67a885ab0a4c37110f8d
Junho <u_innovation@naver.com>
2021-06-17 23:50:06 +0900

    Seoul

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,20 +1,6 @@
 function solution(arr) {
     let answer = 0;
-    let max = Number.MIN_SAFE_INTEGER;
-    for(let x of arr) {
-        let sum = 0, tmp = x;
-        while(tmp) {
-            sum += (tmp % 10);
-            tmp = (Math.floor(tmp / 10));
-        }
-        if(sum > max) {
-            max = sum;
-            answer = x;
-        }else if(sum === max) {
-            if(x > answer) answer = x; 
-        }
-    }
-
+    
     return answer;
 }
 
