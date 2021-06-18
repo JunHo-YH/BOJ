0e26ab27aa831a94e866fb0fd253def5c2aa5a5f
Junho <u_innovation@naver.com>
2021-06-18 20:46:13 +0900

    두 배열 합치기_복습

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,25 +1,17 @@
-function solution(numbers, hand) {
-    var answer = [];
-    let phone = [[1,2,3], [4,5,6], [7,8,9],["*",0,"#"]];
-
-    let currPos = {ax:phone[3][0], ay:phone[3][2]};
-    console.log(currPos);
-    let ax = [-1, 0, 1, 0]; // 행 탐색
-    let ay = [0, 1, 0, -1]; // 열 탐색
-    // for(let x of numbers) {
-    //     if(x === 1 || x === 4 || x === 7) answer.push("L");
-    //     else if(x === 3 || x === 6 || x === ) answer.push("R");
-    //     else {
-    //         for(let i = 0; i < numbers.length; i++) {
-                
-    //         }
-    //     }
-    // }
-
+function solution(arr1, arr2) {
+    let answer = [];
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = p2 = 0;
+    while(p1 < n && p2 < m) {
+        if(arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+        else answer.push(arr2[p2++]);
+    }
+    while(p1 < n) answer.push(arr1[p1++]);
+    while(p2 < m) answer.push(arr2[p2++]);
 
     return answer;
 }
-
-let numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5];
-let hand = "right";
-console.log(solution(numbers, hand));
+let a=[1, 3, 9, 5, 2];
+let b=[3, 2, 5, 7, 8];
+console.log(solution(a, b));
