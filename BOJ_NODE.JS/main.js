5993ab965fb47dc6c00a3938f04151eaf79a8c4a
Junho <u_innovation@naver.com>
2021-06-18 22:17:35 +0900

    연속부분수열1_복습

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,17 +1,21 @@
-function solution(arr1, arr2) {
-    let answer = [];
-    let n = arr1.length;
-    let m = arr2.length;
-    let p1 = p2 = 0;
-    while(p1 < n && p2 < m) {
-        if(arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
-        else answer.push(arr2[p2++]);
+function solution(m, arr) {
+    let answer = 0;
+    let sum = 0;
+    let lt = rt = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(sum === m) {
+            answer++;
+            rt++;
+        }else if(sum > m) {
+            sum -= arr[lt++];
+        }
+        else {
+            sum += arr[rt++] ;
+        }
     }
-    while(p1 < n) answer.push(arr1[p1++]);
-    while(p2 < m) answer.push(arr2[p2++]);
-
     return answer;
 }
-let a=[1, 3, 9, 5, 2];
-let b=[3, 2, 5, 7, 8];
-console.log(solution(a, b));
+
+
+let a=[1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
