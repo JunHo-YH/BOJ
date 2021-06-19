a1e25462406c692be96dc09ee961855a66ec4f0f
Junho <u_innovation@naver.com>
2021-06-20 00:13:50 +0900

    모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,21 +1,37 @@
-function solution(str) {
-    let answer = "";
+function compareMaps(map1, map2) {
+    if(map1.size !== map2.size) return false;
+    for(let [key, val] of map1) {
+        if(!map2.has(key) || map2.get(key) !== val) return false;
+    }
+    return true;
+}
+function solution(s, t) {
+    let answer = 0;
+    let th = new Map();
     let sh = new Map();
-    for(let x of str) {
-        if(sh.has(x)) sh.set(x, sh.get(x) + 1);
-        else sh.set(x, 1);
+    for(let x of t) {
+        if(th.has(x)) th.set(x, th.get(x)+1);
+        else th.set(x, 1);
     }
-    let max = Number.MIN_SAFE_INTEGER;
-    for(let [key, val] of sh) {
-        if(val > max) {
-            max = val;
-            answer = key;
-        }
+    let len = t.length - 1; // 왜?
+    for(let i = 0; i < len; i++) {
+        if(sh.has(s[i])) sh.set(s[i], sh.get(s[i]) + 1);
+        else sh.set(s[i], 1);
+    }
+    let lt = 0;
+    for(let rt = len; rt < s.length; rt++) {
+        if(sh.has(s[rt])) sh.set(s[rt], sh.get(s[rt]) + 1);
+        else sh.set(s[rt], 1);
+        if(compareMaps(sh, th)) answer++;
+        sh.set(s[lt], sh.get(s[lt]) - 1);
+        if(sh.get(s[lt]) === 0) sh.delete(s[lt]);
+        lt++;
+    }    
 
-    }
 
     return answer;
 }
 
-let str="BACBACCACCBDEDE";
-console.log(solution(str));
+let a="bacaAacba";
+let b="abc";
+console.log(solution(a, b));
