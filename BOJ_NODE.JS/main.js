d5257af544d260e60899e2b5cbadbc714d38b016
Junho <u_innovation@naver.com>
2021-06-20 19:46:16 +0900

    올바른 괄호(스택)

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,37 +1,18 @@
-function compareMaps(map1, map2) {
-    if(map1.size !== map2.size) return false;
-    for(let [key, val] of map1) {
-        if(!map2.has(key) || map2.get(key) !== val) return false;
+function solution(str) {
+    let answer = "YES";
+    let stack = [];
+    if(str.length % 2 === 1) return "NO";
+    for(let x of str) {
+        if(x === '(') stack.push(x);
+        else {
+            if(stack.length === 0) return "NO";
+            stack.pop();
+        } 
+        if(stack.length > 0) return "NO";
     }
-    return true;
-}
-function solution(s, t) {
-    let answer = 0;
-    let th = new Map();
-    let sh = new Map();
-    for(let x of t) {
-        if(th.has(x)) th.set(x, th.get(x)+1);
-        else th.set(x, 1);
-    }
-    let len = t.length - 1; // 왜?
-    for(let i = 0; i < len; i++) {
-        if(sh.has(s[i])) sh.set(s[i], sh.get(s[i]) + 1);
-        else sh.set(s[i], 1);
-    }
-    let lt = 0;
-    for(let rt = len; rt < s.length; rt++) {
-        if(sh.has(s[rt])) sh.set(s[rt], sh.get(s[rt]) + 1);
-        else sh.set(s[rt], 1);
-        if(compareMaps(sh, th)) answer++;
-        sh.set(s[lt], sh.get(s[lt]) - 1);
-        if(sh.get(s[lt]) === 0) sh.delete(s[lt]);
-        lt++;
-    }    
 
 
     return answer;
 }
-
-let a="bacaAacba";
-let b="abc";
-console.log(solution(a, b));
+let a="(()(()))(()";
+console.log(solution(a));
