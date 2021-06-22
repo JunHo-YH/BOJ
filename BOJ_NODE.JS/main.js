ae9534efad56dd91a6245598671fae7b1a172951
Junho <u_innovation@naver.com>
2021-06-22 23:48:06 +0900

    공주구하기(큐)

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,16 +1,12 @@
-function solution(str) {
-    let answer = 0;
-    let stack = [];
-    for(let i = 0; i < str.length; i++) {
-        if(str[i] === '(') stack.push(str[i]);
-        else {
-            stack.pop();
-            if(str[i-1] === '(') answer += (stack.length);
-            else answer += 1;
-        }
+function solution(n, k) {
+    let answer;
+    let queue = Array.from({length: n}, (v, i) => i+1);
+    while(queue.length !== 1) {
+        for(let i = 1; i < k; i++) queue.push(queue.shift());
+        queue.shift(); // 3을 외치면 제외 !
     }
+    answer = queue.shift();
     return answer;
 }
 
-let a="()(((()())(())()))(())";
-console.log(solution(a));
+console.log(solution(8, 3));
