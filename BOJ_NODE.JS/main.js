5ed4eea7e64c0dc844337612361855f3c08f80da
Junho <u_innovation@naver.com>
2021-06-22 20:47:01 +0900

    쇠막대기

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,2 +1,16 @@
-const array1 = [1, 2, 'a', '1a'];
-console.log(array1.toString(2))
+function solution(str) {
+    let answer = 0;
+    let stack = [];
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] === '(') stack.push(str[i]);
+        else {
+            stack.pop();
+            if(str[i-1] === '(') answer += (stack.length);
+            else answer += 1;
+        }
+    }
+    return answer;
+}
+
+let a="()(((()())(())()))(())";
+console.log(solution(a));
