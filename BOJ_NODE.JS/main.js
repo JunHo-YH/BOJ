bd9282c3bba38addea23e5469138dbb3a6923981
Junho <u_innovation@naver.com>
2021-06-23 20:43:22 +0900

    버블정렬

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,10 +1,13 @@
 function solution(arr) {
-    let answer;
-    let sArr = Array.from(arr).sort((a,b) => a - b);
-
-    sArr = sArr.join(' ');
-    console.log(sArr);
-
+    let answer = arr;
+    for(let i = 0; i < arr.length-1; i++) {
+        for(let j = 0; j < arr.length-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+            }
+        }
+    }
+    
     return answer;
 }
 
