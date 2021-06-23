de17502eb9035f29a0f8959e881dcddca38ad8fc
Junho <u_innovation@naver.com>
2021-06-23 19:56:19 +0900

    선택정렬

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,16 +1,12 @@
-function solution(need, plan) {
-    let answer = "YES";
-    let queue = need.split('');
-    for(let x of plan) {
-        if(queue.includes(x)) {
-            if(x !== queue.shift()) return 'NO';
-        }
-    }
-    if(queue.length > 0) return 'NO';
-    
+function solution(arr) {
+    let answer;
+    let sArr = Array.from(arr).sort((a,b) => a - b);
+
+    sArr = sArr.join(' ');
+    console.log(sArr);
+
     return answer;
 }
 
-let a="CBA";
-let b="CBDGE";
-console.log(solution(a, b));
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
