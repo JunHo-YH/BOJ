f52b7d2f0972489f245c8e922f4760778b8dd5e4
Junho <u_innovation@naver.com>
2021-06-24 23:14:06 +0900

    NAVER

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,15 +1,31 @@
-function solution(arr) {
-    let answer = arr;
-    for(let i = 0; i < arr.length-1; i++) {
-        for(let j = 0; j < arr.length-i-1; j++) {
-            if(arr[j] > arr[j+1]) {
-                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+function solution(param0) {
+    let answer = [];
+
+    let obj = param0.reduce((cnt, element) => {
+        let name = '';
+        for(let i = 0; i < element.length; i++) {
+            if(element.charAt(i) === '\_' || element.charAt(i) === '\.') {
+                name = element.charAt(i - 1);
+                break;
             }
         }
+        let file = name + element.split('').slice(-2).join('');
+
+        if(cnt[file]) {
+            cnt[file]++;
+        }else {
+            cnt[file] = 1;
+        }
+
+        return cnt;
+    }, {});
+
+    for(let file in obj) {
+        if(obj[file] > 1) {
+            answer.push(file);
+            answer.push(obj[file] + '');
+        }
     }
-    
+
     return answer;
 }
-
-let arr=[13, 5, 11, 7, 23, 15];
-console.log(solution(arr));
