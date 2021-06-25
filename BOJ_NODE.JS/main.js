2ed7abfe3c42b5f562bb85dc7eabccbef5b93d5b
Junho <u_innovation@naver.com>
2021-06-25 23:17:00 +0900

    Change Language

--- BOJ_NODE.JS/main.js
+++ BOJ_NODE.JS/main.js
@@ -1,31 +1,3 @@
-function solution(param0) {
-    let answer = [];
-
-    let obj = param0.reduce((cnt, element) => {
-        let name = '';
-        for(let i = 0; i < element.length; i++) {
-            if(element.charAt(i) === '\_' || element.charAt(i) === '\.') {
-                name = element.charAt(i - 1);
-                break;
-            }
-        }
-        let file = name + element.split('').slice(-2).join('');
-
-        if(cnt[file]) {
-            cnt[file]++;
-        }else {
-            cnt[file] = 1;
-        }
-
-        return cnt;
-    }, {});
-
-    for(let file in obj) {
-        if(obj[file] > 1) {
-            answer.push(file);
-            answer.push(obj[file] + '');
-        }
-    }
-
-    return answer;
+function solution() {
+    // JAVA로 연습
 }
