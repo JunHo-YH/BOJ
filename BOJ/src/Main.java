bf57d374664dd669bc290e093ed653bdb91f0487
Junho <u_innovation@naver.com>
2021-06-26 18:28:42 +0900

    inf_가장 긴 문자열

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class Main {
-	public int solution(String str, char t) {
-		int answer = 0;
-		str = str.toUpperCase(); // 대문자로 변환
-		t = Character.toUpperCase(t); // 문자를 대문자로 변환
-//		System.out.println(str + " " + t);
-//		for(int i = 0; i < str.length(); i++) {
-//			if(str.charAt(i) == t) answer++;
-//		}
-		for(char x : str.toCharArray()) {
-			if(x == t) answer++;
+	public String solution(String str) {
+		String answer = "";
+		int max = Integer.MIN_VALUE, pos;
+		
+		while((pos = str.indexOf(" ")) != -1) {
+			String tmp = str.substring(0, pos);
+			int len = tmp.length();
+			if(len > max) {
+				max = len;
+				answer = tmp;
+			}
+			str = str.substring(pos+1);
+			if(str.length() > max) answer = str;
+		
 		}
+//		while((pos = str.indexOf(' ')) != -1) {
+//			String tmp = str.substring(0, pos);
+//			int len = tmp.length();
+//			if(len > max) {
+//				max = len;
+//				answer = tmp;
+//			}
+//			str = str.substring(pos+1);
+//			System.out.println(str);
+//		}
+//		
 		return answer;
 	}
-	
- 
-    public static void main(String[] args) {
-    	Main T = new Main();
-    	Scanner scan = new Scanner(System.in);
-    	String str = scan.next();
-    	char c = scan.next().charAt(0);
-    	System.out.println(T.solution(str, c));
-    
-    }
+
+	public static void main(String[] args) {
+
+		Main T = new Main();
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		System.out.println(T.solution(str));
+	}
 }
