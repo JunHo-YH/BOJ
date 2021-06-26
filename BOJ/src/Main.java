cbcbfbed2025173f963bc15f063474e71e608e98
Junho <u_innovation@naver.com>
2021-06-26 11:22:58 +0900

    inf_문자찾기

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,15 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Main {
+public class Main {
+	public int solution(String str, char t) {
+		int answer = 0;
+		str = str.toUpperCase(); // 대문자로 변환
+		t = Character.toUpperCase(t); // 문자를 대문자로 변환
+//		System.out.println(str + " " + t);
+//		for(int i = 0; i < str.length(); i++) {
+//			if(str.charAt(i) == t) answer++;
+//		}
+		for(char x : str.toCharArray()) {
+			if(x == t) answer++;
+		}
+		return answer;
+	}
+	
+ 
+    public static void main(String[] args) {
+    	Main T = new Main();
+    	Scanner scan = new Scanner(System.in);
+    	String str = scan.next();
+    	char c = scan.next().charAt(0);
+    	System.out.println(T.solution(str, c));
     
-    public String solution(String[] seoul) {
-        
-    	List<String> arrList = Arrays.asList(seoul);
-        int index = arrList.indexOf("Kim");        
-        
-        return "김서방은 " + index + "에 있다";
-        
-        // vsc git 연결하기
-        
     }
 }
