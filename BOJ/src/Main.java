fe1c2df05de8fbd954dd9b3b7e28cccad62a31d2
Junho <u_innovation@naver.com>
2021-06-27 02:00:29 +0900

    inf_중복문자제거

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -2,35 +2,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public ArrayList<String> solution(int n, String[] str) {
-		ArrayList<String> answer = new ArrayList<String>();
-		for (String x : str) {
-			char[] s = x.toCharArray(); // 문자열을 문자 배열로 변환
-			int lt = 0, rt = x.length() - 1;
-			while (lt < rt) {
-				char tmp = s[lt];
-				s[lt] = s[rt];
-				s[rt] = tmp;
-				lt++;
-				rt--;
-			}
-			String tmp = String.valueOf(s);
-			answer.add(tmp);
-
+	public String solution(String str) {
+		String answer = "";
+		for(int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+			if(i == str.indexOf(str.charAt(i))) answer+=str.charAt(i);
 		}
+		
+		
 		return answer;
 	}
-
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		String[] str = new String[n];
-		for (int i = 0; i < n; i++) {
-			str[i] = scan.next();
-		}
-		for (String s : T.solution(n, str)) {
-			System.out.println(s);
-		}
+		String str = scan.next();
+		System.out.println(T.solution(str));
+	
 	}
 }
