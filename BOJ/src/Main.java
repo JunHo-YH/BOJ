6de1dd065b6bcd7865b4dfa367e3e99147c0fbfb
Junho <u_innovation@naver.com>
2021-06-29 13:23:07 +0900

    가위바위보_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,21 +1,35 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public int solution(String str) {
+	public String solution(int n, int[] arr1, int[] arr2) {
 		String answer = "";
-		for(char x : str.toCharArray()) {
-			if(Character.isDigit(x)) {
-				answer += x;
-			}
+		for(int i = 0; i < n; i++) {
+			if(arr1[i] == arr2[i]) answer += "D";
+			else if(arr1[i] == 1 && arr2[i] == 3) answer += "A";
+			else if(arr1[i] == 2 && arr2[i] == 1) answer += "A";
+			else if(arr1[i] == 3 && arr2[i] == 2) answer += "A";
+			else answer += "B";
+			
 		}
-		return Integer.parseInt(answer);
+		
+		return answer;
 		
 	}
+
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
-		System.out.println(T.solution(str));
+		int n = scan.nextInt();
+		int[] arr1 = new int[n];
+		for(int i = 0; i < arr1.length; i++) {
+			arr1[i] = scan.nextInt();
+		}
+		int[] arr2 = new int[n];
+		for(int i = 0; i < arr2.length; i++) {
+			arr2[i] = scan.nextInt();
+		}
+		for(char x : T.solution(n, arr1, arr2).toCharArray()) System.out.println(x);
+	
 	}
 }
-	
