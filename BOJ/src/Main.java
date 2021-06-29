cfcf7e23bac4857273c239c22e1a4fb40b185e1e
Junho <u_innovation@naver.com>
2021-06-30 08:21:32 +0900

    뒤집은 소수_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,14 +1,29 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public int solution(int n) {
-		int answer = 0;
-		int[] ch = new int[n+1]; // 해당 수까지 포함하려면 index+1
-		for(int i = 2; i <= n; i++) {
-			if(ch[i] == 0) {
-				answer++;
-				for(int j = i; j <= n; j = j + i) ch[j] = 1;
+	
+	public boolean isPrime(int num) {
+		if(num == 1) return false;
+		for(int i = 2; i < num; i++) {
+			if(num % i == 0) return false;
+		}
+		
+		return true;
+	}
+	
+	
+	public ArrayList<Integer> solution(int n, int[] arr) {
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		for(int x : arr) {
+			int tmp = x;
+			int res = 0;
+			while(tmp > 0) {
+				int t = tmp % 10;
+				res = res * 10 + t;
+				tmp = tmp / 10;
 			}
+			if(isPrime(res)) answer.add(res);
 		}
 		
 		return answer;
@@ -17,6 +32,10 @@ public class Main {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		System.out.println(T.solution(n));
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		for(int x : T.solution(n, arr)) System.out.print(x + " ");
 	}
 }
