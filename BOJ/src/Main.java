c4e2b001016d23aa19676aa6c055394a7b31fd34
Junho <u_innovation@naver.com>
2021-06-29 13:52:10 +0900

    피보나치 수열_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,35 +1,24 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public String solution(int n, int[] arr1, int[] arr2) {
-		String answer = "";
-		for(int i = 0; i < n; i++) {
-			if(arr1[i] == arr2[i]) answer += "D";
-			else if(arr1[i] == 1 && arr2[i] == 3) answer += "A";
-			else if(arr1[i] == 2 && arr2[i] == 1) answer += "A";
-			else if(arr1[i] == 3 && arr2[i] == 2) answer += "A";
-			else answer += "B";
-			
+	public int[] solution(int n) {
+		int[] answer = new int[n];
+		answer[0] = 1;
+		answer[1] = 1;
+		
+		for(int i = 2; i < n; i++) {
+			answer[i] = answer[i-2] + answer[i-1];
 		}
-		
 		return answer;
-		
+
 	}
 
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int[] arr1 = new int[n];
-		for(int i = 0; i < arr1.length; i++) {
-			arr1[i] = scan.nextInt();
-		}
-		int[] arr2 = new int[n];
-		for(int i = 0; i < arr2.length; i++) {
-			arr2[i] = scan.nextInt();
-		}
-		for(char x : T.solution(n, arr1, arr2).toCharArray()) System.out.println(x);
-	
+		for (int x : T.solution(n))
+			System.out.print(x + " ");
+
 	}
 }
