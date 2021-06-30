9c8271f58fce118060f90d0642963cb05d62ac3a
Junho <u_innovation@naver.com>
2021-06-30 09:23:09 +0900

    등수구하기_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -2,40 +2,32 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	
-	public boolean isPrime(int num) {
-		if(num == 1) return false;
-		for(int i = 2; i < num; i++) {
-			if(num % i == 0) return false;
-		}
-		
-		return true;
-	}
-	
-	
-	public ArrayList<Integer> solution(int n, int[] arr) {
-		ArrayList<Integer> answer = new ArrayList<Integer>();
-		for(int x : arr) {
-			int tmp = x;
-			int res = 0;
-			while(tmp > 0) {
-				int t = tmp % 10;
-				res = res * 10 + t;
-				tmp = tmp / 10;
+
+	public int[] solution(int n, int[] arr) {
+		int[] answer = new int[n];
+		for (int i = 0; i < n; i++) {
+			int lv = 1;
+			for (int k = 0; k < n; k++) {
+				if (arr[i] < arr[k])
+					lv++;
 			}
-			if(isPrime(res)) answer.add(res);
+			answer[i] = lv;
 		}
-		
+
 		return answer;
 	}
+
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = scan.nextInt();
 		}
-		for(int x : T.solution(n, arr)) System.out.print(x + " ");
+		for (int x : T.solution(n, arr)) {
+			System.out.print(x + " ");
+		}
+
 	}
 }
