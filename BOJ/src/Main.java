a71ef9961c21c1c94d51fec41be9350d542a11d1
Junho <u_innovation@naver.com>
2021-06-30 12:57:05 +0900

    최대합_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -3,17 +3,27 @@ import java.util.Scanner;
 
 public class Main {
 
-	public int[] solution(int n, int[] arr) {
-		int[] answer = new int[n];
-		for (int i = 0; i < n; i++) {
-			int lv = 1;
-			for (int k = 0; k < n; k++) {
-				if (arr[i] < arr[k])
-					lv++;
+	public int solution(int n, int[][] arr) {
+		int answer = Integer.MIN_VALUE;
+		int sum1 = 0, sum2 = 0;
+		for(int i = 0; i < n; i++) {
+			sum1 = sum2 = 0;
+			for(int k = 0; k < n; k++) {
+				sum1 += arr[i][k];
+				sum2 += arr[k][i];
 			}
-			answer[i] = lv;
+			answer = Math.max(answer, sum1);
+			answer = Math.max(answer, sum2);
 		}
-
+		sum1 = sum2 = 0;
+		for(int i = 0; i < n; i++) {
+			sum1 += arr[i][i];
+			sum2 += arr[i][n-i-1];
+		}
+		answer = Math.max(answer, sum1);
+		answer = Math.max(answer, sum2);
+		
+		
 		return answer;
 	}
 
@@ -21,13 +31,13 @@ public class Main {
 		Main T = new Main();
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = scan.nextInt();
+		int[][] arr = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = scan.nextInt();
+			}
 		}
-		for (int x : T.solution(n, arr)) {
-			System.out.print(x + " ");
-		}
+		System.out.println(T.solution(n, arr));
 
 	}
 }
