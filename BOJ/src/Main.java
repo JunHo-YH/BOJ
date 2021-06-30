854403f7f1893d68ed431e939997395203541fbd
Junho <u_innovation@naver.com>
2021-06-30 20:05:30 +0900

    봉우리_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,30 +1,27 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+	int[] dx = { -1, 0, 1, 0 };
+	int[] dy = { 0, 1, 0, -1 };
 	public int solution(int n, int[][] arr) {
-		int answer = Integer.MIN_VALUE;
-		int sum1 = 0, sum2 = 0;
+		int answer = 0;
 		for(int i = 0; i < n; i++) {
-			sum1 = sum2 = 0;
-			for(int k = 0; k < n; k++) {
-				sum1 += arr[i][k];
-				sum2 += arr[k][i];
+			for(int j = 0; j < n; j++) {
+				boolean flag = true;
+				for(int k = 0; k < 4; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					if(nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] > arr[i][j]) {
+						flag = false;
+						break;
+					}
+				}
+				if(flag) answer++;
 			}
-			answer = Math.max(answer, sum1);
-			answer = Math.max(answer, sum2);
 		}
-		sum1 = sum2 = 0;
-		for(int i = 0; i < n; i++) {
-			sum1 += arr[i][i];
-			sum2 += arr[i][n-i-1];
-		}
-		answer = Math.max(answer, sum1);
-		answer = Math.max(answer, sum2);
-		
 		
 		return answer;
+		
 	}
 
 	public static void main(String[] args) {
@@ -32,9 +29,9 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int[][] arr = new int[n][n];
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				arr[i][j] = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k < n; k++) {
+				arr[i][k] = scan.nextInt();
 			}
 		}
 		System.out.println(T.solution(n, arr));
