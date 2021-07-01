454463bd76395be017ec7f82981ff78d8080d26e
Junho <u_innovation@naver.com>
2021-07-01 16:21:00 +0900

    연속된 두 수의 합_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,40 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	public int solution(int n, int[][] arr) {
-		int answer = 0, max = Integer.MIN_VALUE;
-		for (int i = 1; i <= n; i++) {
-			int cnt = 0;
-			for (int j = 1; j <= n; j++) {
-//				if(i != j) 
-				for (int k = 1; k <= 5; k++) {
-					if (arr[i][k] == arr[j][k]) {
-						cnt++;
-						break;
-					}
-				}
-			}
-			if (cnt > max) {
-				max = cnt;
-				answer = i;
-			}
-
+	public int solution(int n) {
+		int answer = 0, sum = 0;
+		int m = n / 2 +1;
+		int[] arr = new int[m];
+		for(int i = 0; i < m; i++) {
+			arr[i] = i+1;
 		}
+		int lt = 0;
+		for(int rt = 0; rt < m; rt++) {
+			sum += arr[rt];
+			if(sum == n) answer++;
+			while(sum >= n) {
+				sum -= arr[lt++];
+				if(sum == n) answer++;
+			}
+		}
+		
+		
 		return answer;
-
 	}
 
 	public static void main(String[] args) {
 		Main T = new Main();
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		int[][] arr = new int[n + 1][6]; // 0번은 버림 => 1부터 시작
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= 5; j++) {
-				arr[i][j] = scan.nextInt();
-			}
-		}
-		System.out.println(T.solution(n, arr));
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(T.solution(n));
 	}
 }
