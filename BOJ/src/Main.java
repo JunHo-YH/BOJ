0c637725343cfa8f2e2dace88def06225f0b2300
Junho <u_innovation@naver.com>
2021-07-01 16:23:02 +0900

    두 수의 합(수학)_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -2,22 +2,15 @@ import java.util.*;
 
 public class Main {
 	public int solution(int n) {
-		int answer = 0, sum = 0;
-		int m = n / 2 +1;
-		int[] arr = new int[m];
-		for(int i = 0; i < m; i++) {
-			arr[i] = i+1;
-		}
-		int lt = 0;
-		for(int rt = 0; rt < m; rt++) {
-			sum += arr[rt];
-			if(sum == n) answer++;
-			while(sum >= n) {
-				sum -= arr[lt++];
-				if(sum == n) answer++;
+		int answer = 0, cnt=1;
+		n--;
+		while(n > 0) {
+			cnt++;
+			n = n - cnt;
+			if(n % cnt == 0) {
+				answer++;
 			}
 		}
-		
 		
 		return answer;
 	}
