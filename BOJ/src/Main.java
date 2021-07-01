8f2c1baaa2a6dd816c128dbd2d80ae6b3f6e0b3c
Junho <u_innovation@naver.com>
2021-07-01 20:21:44 +0900

    최대길이_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class Main {
-	public int solution(int n) {
-		int answer = 0, cnt=1;
-		n--;
-		while(n > 0) {
-			cnt++;
-			n = n - cnt;
-			if(n % cnt == 0) {
-				answer++;
+	public int solution(int n, int k, int[] arr) {
+		int answer = 0, cnt = 0, lt = 0;
+		for(int rt = 0; rt < n; rt++) {
+			if(arr[rt] == 0) cnt++;
+			while(cnt > k) {
+				if(arr[lt] == 0) cnt--;
+				lt++;
 			}
+			answer = Math.max(answer, rt-lt+1);
+			System.out.print(rt-lt+1 + " ");
 		}
-		
 		return answer;
 	}
 
@@ -19,6 +19,11 @@ public class Main {
 		Main T = new Main();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(T.solution(n));
+		int k = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(T.solution(n, k, arr));
 	}
 }
