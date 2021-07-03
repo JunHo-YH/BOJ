6955a3591964c96049bfbee10b4f301bddc8da2a
Junho <u_innovation@naver.com>
2021-07-03 10:49:55 +0900

    올바른 괄호_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,36 +1,28 @@
 import java.util.*;
 
 public class Main {
-	public ArrayList<Integer> solution(int n, int m, int[] arr) {
-		ArrayList<Integer> answer = new ArrayList<Integer>();
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for(int i = 0; i < m-1; i++) {
-			map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+	public String solution(String str) {
+		String answer = "YES";
+		Stack<Character> stack = new Stack<Character>(); // Char형 스택 선언
+		
+		
+		for(char x : str.toCharArray()) {
+			if(x == '(') stack.push(x);
+			else {
+				if(stack.isEmpty()) return "NO";
+				else stack.pop();
+			}
 		}
-		int lt = 0;
-		for(int rt = m-1; rt < n; rt++) {
-			map.put(arr[rt], map.getOrDefault(arr[rt], 0)+1);
-			answer.add(map.size());
-			map.put(arr[lt], map.get(arr[lt])-1);
-			if(map.get(arr[lt]) == 0) map.remove(arr[lt]);
-			lt++;
-		}
+		if(!stack.isEmpty()) return "NO";
 		
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		Main T = new Main();
+		Main M = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int[] arr = new int[n];
-
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		for (int x : T.solution(n, m, arr)) {
-			System.out.print(x + " ");
-		}
+		String str = sc.next();
+		
+		System.out.println(M.solution(str));
 	}
 }
