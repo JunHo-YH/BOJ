cf108548453d5f2f3adc0029649becad9b91b8c7
Junho <u_innovation@naver.com>
2021-07-05 00:26:33 +0900

    교육과정 설계

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,24 +1,26 @@
 import java.util.*;
 
 public class Main {
-	public int solution(int n, int k) {
-		int answer = 0;
-		Queue<Integer> Q = new LinkedList<Integer>();
-		for(int i = 1; i <= n; i++) Q.offer(i);
-		while(!Q.isEmpty()) {
-			for(int i = 1; i < k; i++) Q.offer(Q.poll());
-			Q.poll();
-			if(Q.size() == 1) answer = Q.poll();
+	public String solution(String s1, String s2) {
+		String answer = "YES";
+		Queue<Character> Q = new LinkedList<Character>();
+		for(char x : s1.toCharArray()) Q.offer(x);
+		for(char x : s2.toCharArray()) {
+			if(Q.contains(x)) {
+				if(Q.poll() != x) return "NO";
+			}
 		}
+		if(!Q.isEmpty()) return "NO";
+		
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		Main M = new Main();
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-		System.out.println(M.solution(n, k));
-	
+		String s1 = sc.next();
+		String s2 = sc.next();
+		System.out.println(M.solution(s1, s2));
+
 	}
 }
