bf0dad39e17a2a07fe2913b37e70960b6de6d7ea
Junho <u_innovation@naver.com>
2021-07-04 23:30:34 +0900

    공주 구하기_INF

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,27 +1,24 @@
 import java.util.*;
 
 public class Main {
-	public int solution(String str) {
+	public int solution(int n, int k) {
 		int answer = 0;
-		Stack<Character> stack = new Stack<Character>();
-		for(int i = 0; i < str.length(); i++) {
-			if(str.charAt(i) == '(') stack.push('(');
-			else {
-				stack.pop();
-				if(str.charAt(i-1) == '(') answer += stack.size();
-//				else if(str.charAt(i-1) == ')') answer++;
-				else answer++;
-			}
+		Queue<Integer> Q = new LinkedList<Integer>();
+		for(int i = 1; i <= n; i++) Q.offer(i);
+		while(!Q.isEmpty()) {
+			for(int i = 1; i < k; i++) Q.offer(Q.poll());
+			Q.poll();
+			if(Q.size() == 1) answer = Q.poll();
 		}
 		return answer;
-		
-
 	}
 
 	public static void main(String[] args) {
 		Main M = new Main();
 		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		System.out.println(M.solution(str));
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.println(M.solution(n, k));
+	
 	}
 }
