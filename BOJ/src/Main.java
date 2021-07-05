1c739d7c95378d8562e53ed6ab5058637ba5d859
Junho <u_innovation@naver.com>
2021-07-05 17:14:20 +0900

    중복확인

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,37 +1,25 @@
 import java.util.*;
 
 public class Main {
-	public int[] solution(int size, int n, int[] arr) {
-		int[] cache = new int[size];
-		for(int x : arr) {
-			int pos = -1;
-			for(int i = 0; i < size; i++) if(x == cache[i]) pos = i;
-			if(pos == -1) {
-				for(int i = size-1; i >= 1; i--) {
-					cache[i] = cache[i-1];
-				}
-				cache[0] = x;
-			} else {
-				for(int i = pos; i >= 1; i--) {
-					cache[i] = cache[i-1];
-				}
-				cache[0] = x;
-			}
-		
+	public String solution(int n, int[] arr) {
+		String answer = "U";
+		Queue<Integer> Q = new LinkedList<Integer>();
+		for(int i = 0; i < arr.length; i++) Q.offer(arr[i]);
+		while(!Q.isEmpty()) {
+			if(Q.contains(Q.poll())) return "D";
 		}
-		
-		return cache;
+		return answer;
+
 	}
 
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		int s = kb.nextInt();
 		int n = kb.nextInt();
 		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++) {
 			arr[i] = kb.nextInt();
-		for (int x : T.solution(s, n, arr))
-			System.out.print(x + " ");
+		}
+		System.out.println(T.solution(n, arr));
 	}
 }
