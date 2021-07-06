af2e13ffe0b550bbfd04c38aca11bccaf35b2216
Junho <u_innovation@naver.com>
2021-07-06 22:41:28 +0900

    팩토리얼_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,28 +1,12 @@
 import java.util.*;
-class Point implements Comparable<Point> {
-	public int x, y;
-	public Point(int x , int y) {
-		this.x = x;
-		this.y = y;
+class Main {
+	public int DFS(int n) {
+		if(n == 1) return 1;
+		else return n * DFS(n-1);
 	}
-	@Override
-	public int compareTo(Point o) {
-		if(this.x == o.x) return this.y - o.y; // 오름차순
-		else return this.x - o.x;
-	}
-}
-class Main {	
-	public static void main(String[] args){
-		Scanner kb = new Scanner(System.in);
-		int n=kb.nextInt();
-		ArrayList<Point> arr = new ArrayList<Point>();
-		for(int i = 0; i < n; i++) {
-			int x = kb.nextInt();
-			int y = kb.nextInt();
-			arr.add(new Point(x, y));
-		}
-		
-		Collections.sort(arr);
-		for(Point o : arr) System.out.println(o.x+" "+o.y);
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		System.out.println(T.DFS(5));
 	}
 }
