ee658a65231e364276a0dfd85bbe4d4a8c203f20
Junho <u_innovation@naver.com>
2021-07-07 00:07:36 +0900

    피보나치 재귀(메모이제이션)

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,12 +1,19 @@
 import java.util.*;
 class Main {
+	static int[] fibo;
 	public int DFS(int n) {
-		if(n == 1) return 1;
-		else return n * DFS(n-1);
+		if(fibo[n] > 0) return fibo[n];
+		if(n == 1) return fibo[n] = 1;
+		else if(n == 2) return fibo[n] = 1;
+		else return fibo[n] = DFS(n-2) + DFS(n-1);
+		
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
-		System.out.println(T.DFS(5));
+		int n = 45;
+		fibo = new int[n+1];
+		T.DFS(n);
+		for(int i = 1; i <=n; i++) System.out.print(fibo[i] + " ");
 	}
 }
