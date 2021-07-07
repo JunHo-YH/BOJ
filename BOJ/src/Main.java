41f5e8ff1107cc797ce9fd37715eaf49ff314f3d
Junho <u_innovation@naver.com>
2021-07-07 21:07:32 +0900

    11021_BOJ

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,20 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
-
+import java.io.IOException;
+ 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] str = br.readLine().split(" ");
-		int A = Integer.parseInt(str[0]);
-		int B = Integer.parseInt(str[1]);
+	public static void main(String args[]) throws IOException {
+ 
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println((A > B) ? ">" : (A < B ? "<" : "=="));
-		
+		int a = Integer.parseInt(br.readLine());
+ 
+		StringTokenizer st;
+		for (int i = 1; i <= a; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			System.out.println("Case #" + i + ": " 
+			+(Integer.parseInt(st.nextToken())
+			+Integer.parseInt(st.nextToken())));
+		}
+		br.close();
 	}
-
+ 
 }
