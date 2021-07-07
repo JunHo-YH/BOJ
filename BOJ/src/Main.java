7b9d057c85f791b2b48e4072e30a4df6f1212e44
Junho <u_innovation@naver.com>
2021-07-07 16:49:24 +0900

    두 수 비교하기

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,31 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
-public class Main{
- 
+import java.util.StringTokenizer;
+
+
+public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		
-		br.close();
 
-		StringBuilder sb = new StringBuilder();
-		sb.append(A * (B%10));
-		sb.append('\n');
+		String[] str = br.readLine().split(" ");
+		int A = Integer.parseInt(str[0]);
+		int B = Integer.parseInt(str[1]);
 		
-		sb.append(A * ((B%100)/10));
-		sb.append('\n');
-        
-		sb.append(A * (B/100));
-		sb.append('\n');
-        
-		sb.append(A * B);
- 
-		System.out.print(sb);
-        
+		System.out.println((A > B) ? ">" : (A < B ? "<" : "=="));
+		
 	}
- 
+
 }
