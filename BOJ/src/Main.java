8ae6ee5ecc5369f711882f3dd89d49ff502453b2
Junho <u_innovation@naver.com>
2021-07-07 09:59:51 +0900

    이진트리순회(DFS : Depth-First Search)_inf

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,19 +1,34 @@
 import java.util.*;
-class Main {
-	static int[] fibo;
-	public int DFS(int n) {
-		if(fibo[n] > 0) return fibo[n];
-		if(n == 1) return fibo[n] = 1;
-		else if(n == 2) return fibo[n] = 1;
-		else return fibo[n] = DFS(n-2) + DFS(n-1);
-		
-	}
-	
-	public static void main(String[] args) {
-		Main T = new Main();
-		int n = 45;
-		fibo = new int[n+1];
-		T.DFS(n);
-		for(int i = 1; i <=n; i++) System.out.print(fibo[i] + " ");
+class Node {
+	int data;
+	Node lt, rt;
+	public Node(int val) {
+		int data = val;
+		lt = rt = null;
 	}
 }
+  
+public class Main{ 
+    Node root; 
+    public void DFS(Node root){ 
+        if(root==null) 
+            return; 
+        else{
+			DFS(root.lt);
+			DFS(root.rt);
+			System.out.print(root.data+" ");
+		}
+    } 
+  
+    public static void main(String args[]) { 
+        Main tree=new Main(); 
+        tree.root=new Node(1); 
+        tree.root.lt=new Node(2); 
+        tree.root.rt=new Node(3); 
+        tree.root.lt.lt=new Node(4); 
+        tree.root.lt.rt=new Node(5); 
+		tree.root.rt.lt=new Node(6); 
+        tree.root.rt.rt=new Node(7);
+        tree.DFS(tree.root); 
+    } 
+} 
