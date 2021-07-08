594f90813acffc1fcbef86e0bf224a0dc124d7a4
Junho <u_innovation@naver.com>
2021-07-08 20:54:23 +0900

    3052_BOJ

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,27 +1,17 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-	public static void main(String args[]) throws IOException {
- 
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int n = Integer.parseInt(st.nextToken());
-		int x = Integer.parseInt(st.nextToken());
-		int[] arr = new int[n];
-		
-		StringBuilder sb = new StringBuilder();
-		st = new StringTokenizer(br.readLine()," ");
-		for(int i = 0; i < n; i++) {
-			int value = Integer.parseInt(st.nextToken());
-			
-			if(value < x) sb.append(value).append(" ");
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HashSet<Integer> h = new LinkedHashSet<Integer>();
+		for(int i = 0; i < 10; i++) {
+			h.add(Integer.parseInt(br.readLine()) % 42);
 		}
-		
-		br.close();
-		System.out.println(sb);
+		System.out.println(h.size());
 	}
- 
+	
 }
