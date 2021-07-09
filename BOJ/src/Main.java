110e33751b072279388c54436a05f13b5a4bd9f4
Junho <u_innovation@naver.com>
2021-07-09 17:39:15 +0900

    핸드폰 번호 가리기_PRO

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,17 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		HashSet<Integer> h = new LinkedHashSet<Integer>();
-		for(int i = 0; i < 10; i++) {
-			h.add(Integer.parseInt(br.readLine()) % 42);
-		}
-		System.out.println(h.size());
-	}
-	
+class Main {
+    public String solution(String phone_number) {
+        String answer = "";
+        answer =  phone_number.substring(0,phone_number.length()-4).replaceAll("[0-9]", "*");
+        String str = phone_number.substring(phone_number.length()-4);
+        return answer + str;
+    }
 }
