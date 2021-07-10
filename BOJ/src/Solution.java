c849812687593889860305fc93c4a13e1be45853
Junho <u_innovation@naver.com>
2021-07-10 23:52:58 +0900

    문자열을 정수로 바꾸기_pro

--- BOJ/src/Solution.java
+++ BOJ/src/Solution.java
@@ -1,39 +1,13 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
-public class Solution {
-	public int solution(int[] gift_cards, int[] wants) {
+class Solution {
+    public int solution(String s) {
         int answer = 0;
-        int count = 0;
-        for(int i = 0; i < gift_cards.length; i++) {
-            for (int k = 0; k < wants.length; k++) {
-                if (gift_cards[i] == wants[k]) {
-                    count++;
-                }
-            }
-        }
-        
-        
-        
         return answer;
     }
 
-	public static void main(String[] args) {
-		 
-	    Solution sol = new Solution();
-	    Scanner scan = new Scanner(System.in);
-	    int people = scan.nextInt();
-	    int[] gift_cards = new int[people];
-	    int[] wants = new int[people];
-	    for (int i = 0; i < gift_cards.length; i++) {
-	        gift_cards[i] = scan.nextInt();
-	    }
-	    for (int i = 0; i < gift_cards.length; i++) {
-	    	wants[i] = scan.nextInt();
-	    	
-	    	
-	    }
-	       
-	    System.out.println(sol.solution(gift_cards, wants) );
-	}
-
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        System.out.println(T.solution("-1234"));
+    }
 }
