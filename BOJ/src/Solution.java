93d3a8dfcea924aaa4fb04c31fae96c60b07d08c
Junho <u_innovation@naver.com>
2021-07-11 23:05:06 +0900

    p

--- BOJ/src/Solution.java
+++ BOJ/src/Solution.java
@@ -1,13 +1,33 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
+    public int[] solution(int[] deposit) {
+        int[] answer = new int[deposit.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int bank : deposit) {
+            int calc = 0;
+            if(bank >= 0) stack.push(bank);
+            else {
+                while(calc <= 0) {
+                int money = stack.pop();
+                calc =  bank + money;
+                bank = calc;
+                }
+                stack.push(calc);
+            }
+        }
+        for(int i = 0; i <stack.size(); i++) {
+            answer[i] = stack.get(i);
+        }
+
         return answer;
     }
 
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
     public static void main(String[] args) {
-        Solution T = new Solution();
-        System.out.println(T.solution("-1234"));
+        Solution rs = new Solution();
+
+        int[] arr = {500,1000,-300,200,-400,100,-100};
+        System.out.println(rs.solution(arr));
     }
 }
