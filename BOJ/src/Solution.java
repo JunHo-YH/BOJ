95d50e22cb167b5c95e6a106054b65bb94026ad7
Junho <u_innovation@naver.com>
2021-07-12 23:56:43 +0900

    BOJ

--- BOJ/src/Solution.java
+++ BOJ/src/Solution.java
@@ -1,7 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] deposit) {
-    	return 0;
-    }    
+	public int[] solution(int[] arr) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    	Main T = new Main();
+        int[] arr = {4,3,2,1};
+        for(long a : T.solution(arr)) System.out.println(a);
+
+    }   
 }
