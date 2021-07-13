32bc8df151015e29e876306f203ea2e02b4c5e12
Junho <u_innovation@naver.com>
2021-07-13 21:21:00 +0900

    function_BOJ

---
+++ BOJ/src/Test.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+
+public class Test {
+
+	public long sum(int[] a) {
+		long sum = 0;
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+
+}
