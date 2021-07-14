e3f932020548a0649657bae8467c616c4abf0020
Junho <u_innovation@naver.com>
2021-07-14 15:48:04 +0900

    단어 공부_BOJ

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,17 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int find = 97;
 		String str = br.readLine();
-		for (int i = 97; i < 123; i++) {
-			System.out.print(str.indexOf(i) + " ");
-		}
+		str = str.toUpperCase();
+		int max = Integer.MIN_VALUE;
+		char answer = ' ';
 
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(char x : str.toCharArray()) {
+			map.put(x, map.getOrDefault(x, 0)+1);
+		}
+		for(char key : map.keySet()) {
+			if(map.get(key) == max) answer = '?';
+			else if(map.get(key) > max) {
+				max = map.get(key);
+				answer = key;
+			}
+		}
+		System.out.println(answer);
 	}
 
 }
