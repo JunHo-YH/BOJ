6ee96745d468146f671ddad5571332af8f7f0863
Junho <u_innovation@naver.com>
2021-07-16 23:32:01 +0900

    BOJ_NEXT

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -6,24 +6,6 @@ import java.util.HashMap;
 public class Main {
 	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		str = str.toUpperCase();
-		int max = Integer.MIN_VALUE;
-		char answer = ' ';
-
-		HashMap<Character, Integer> map = new HashMap<>();
-		for(char x : str.toCharArray()) {
-			map.put(x, map.getOrDefault(x, 0)+1);
-		}
-		for(char key : map.keySet()) {
-			if(map.get(key) == max) answer = '?';
-			else if(map.get(key) > max) {
-				max = map.get(key);
-				answer = key;
-			}
-		}
-		System.out.println(answer);
 	}
 
 }
