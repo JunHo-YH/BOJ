4982033f32d38e9df9174abc2f2b545c7820d78e
Junho <u_innovation@naver.com>
2021-07-18 22:07:25 +0900

    BOJ_한수_BufferedReader

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,41 +1,35 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		System.out.print(arithmetic_sequence(in.nextInt()));
-		in.close();
- 
-		
-	}
- 
+	
 	public static int arithmetic_sequence(int num) {
 		int cnt = 0; // 한수 카운팅
- 
-		if (num < 100) {
-			return num;
-		}
- 
+		
+		if(num < 100) return num;
 		else {
-			cnt = 99;
-			if (num == 1000) { // 예외처리 필수
-				num = 999;
-			}
- 
-			for (int i = 100; i <= num; i++) {
-				int hun = i / 100; // 백의 자릿수
-				int ten = (i / 10) % 10; // 십의 자릿수
+			cnt = 99; // 100 이상일 경우 최소 99개이므로
+			
+			if(cnt == 1000) num = 999; // 예외처리 필수 -> 1000까지 입력가능 && 1000은 등차수열이 아님
+			
+			for(int i = 100; i <= num; i++) {
+				int hun = i / 100;
+				int ten = (i / 10) % 10;
 				int one = i % 10;
- 
-				if ((hun - ten) == (ten - one)) { // 각 자릿수가 수열을 이루면
-					cnt++;
-				}
+				
+				if((hun - ten) == (ten - one)) cnt++; // 각 자릿수가 수열을 이루면
 			}
 		}
- 
+		
 		return cnt;
 	}
- 
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(arithmetic_sequence(Integer.parseInt(br.readLine())));
+	}
+	
 }
