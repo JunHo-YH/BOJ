2002f8a595c605ef4c9f0479d1e09c0d47fa2964
Junho <u_innovation@naver.com>
2021-07-20 00:04:05 +0900

    상수_boj_더 효율적인 코드

--- BOJ/src/Main.java
+++ BOJ/src/Main.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		String str2 = br.readLine();
-		StringBuilder sb = new StringBuilder();
-		StringBuilder sb2 = new StringBuilder();
-		int num1 = Integer.parseInt(sb.append(str).reverse().toString());
-		int num2 = Integer.parseInt(sb2.append(str2).reverse().toString());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int num1 = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int num2 = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
 		System.out.println(num1 > num2 ? num1 : num2);
 	
 	
